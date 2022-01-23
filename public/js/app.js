f9ba3d23ebfee12aa3e22508574bd3b1da54f81f
Mudit Khandelwal <gupta.mudit2299@gmail.com>
2022-01-24 00:57:57 +0530

    Remove unnecessary console.log lines

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,7 @@
-console.log('Client side file is loading!!!')
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const msgOne = document.querySelector('#msg-1')
 const msgTwo = document.querySelector('#msg-2')
-
-
-
-
-
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
